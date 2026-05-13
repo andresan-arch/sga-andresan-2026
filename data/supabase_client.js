98f0fd2fc87f584db8071344440bbdcf17e15206
Andresan <andresan@example.com>
2026-05-13 08:54:40 -0500

    a

--- data/supabase_client.js
+++
@@ -1,5 +0,0 @@
-window.SB_URL = "https://pmfrdrihzdhhjxokabjm.supabase.co";
-window.SB_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBtZnJkcmloemRoaGp4b2thYmptIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU4NTA5OTIsImV4cCI6MjA5MTQyNjk5Mn0.FnQWmxg0p4oUgAQBVhoy6wqIMOk3qk4pifI2voeQvV0";
-
-// Instancia global
-window.sb = window.supabase ? window.supabase.createClient(window.SB_URL, window.SB_KEY) : null;
--- shared/constants.js
+++
@@ -1,6 +0,0 @@
-window.APP_CONFIG = {
-    LOGO_URL: "https://pmfrdrihzdhhjxokabjm.supabase.co/storage/v1/object/public/assets/andresanbordessinfondo.png",
-    FONDO_URL: "https://pmfrdrihzdhhjxokabjm.supabase.co/storage/v1/object/public/assets/DSC06323.JPG",
-    DEFAULT_YEAR: 2026,
-    DEFAULT_PERIOD: 1
-};
